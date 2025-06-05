3e4212ad68772eb919b430506fd93e3f1038f150
Izadora Alves <alveszadora02@gmail.com>
2025-06-05 16:54:29 -0300

    Teste #1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,16 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Home } from "@/pages/Home";
-import { About } from "@/pages/About";
+import { Routes, Route } from "react-router-dom";
+
+import Header from "./components/Header";
+import Home from "./pages/Home";
 
 export const App = () => {
   return (
-    <BrowserRouter>
+    <div>
+      <Header />
+      
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
       </Routes>
-    </BrowserRouter>
+    </div>
   );
 };
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Página Inicial</h1>
+      <p>Conteúdo da home...</p>
+    </div>
+  );
+};
+
+export default Home; // Exportação padrão
